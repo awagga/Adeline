2b0c4a32d9dcfc3c1fc1808d817950ae43963dac
awagga <awagga@mail.com>
2022-06-03 14:35:28 +0100

    inline timeout error

--- adeline.js
+++ adeline.js
@@ -6,15 +6,13 @@ const P = console.log;
 const F = (o) => { return "```" + o.slice(0, 1993) + "```" };
 const c = new Eris(process.env.adeline);
 
-const e = "EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds";
-
 async function A(i) {
   let p = spawn("./apl", [i]);
   let o = "";
   
   p.stdout.on("data",(d)=>{ o+=d.toString()});
          p.on("exit",( )=>{ clearTimeout(t)});
-  var t = setTimeout(( )=>{ o+=e
+  var t = setTimeout(( )=>{ o+="EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds"
     p.kill()}, 10000);
   
   await once(p, "exit");return o;
