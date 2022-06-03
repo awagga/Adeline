562045a594576178a7c049341ce5e57298b347b9
awagga <awagga@mail.com>
2022-06-03 14:26:51 +0100

    Buffer timeout error

--- adeline.js
+++ adeline.js
@@ -6,23 +6,26 @@ const P = console.log;
 const F = (o) => { return "```" + o.slice(0, 1993) + "```" };
 const c = new Eris(process.env.adeline);
 
-async function A(m,i) {
+const e = "EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds";
+
+async function A(i) {
   let p = spawn("./apl", [i]);
-
-  let o = "";p.stdout.on("data", (d) => { o += d.toString() });
-
-  var timeout = setTimeout(() => { p.kill();c.createMessage(m.channel.id,
-    F("EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds")) }, 10000);
+  let o = "";
   
-  p.on("exit", () => { clearTimeout(timeout) });await once(p, "exit");return o;
+  p.stdout.on("data",(d)=>{ o+=d.toString()});
+         p.on("exit",( )=>{ clearTimeout(t)});
+  var t = setTimeout(( )=>{ o+=e
+    p.kill()}, 10000);
+  
+  await once(p, "exit");return o;
 }
 
 async function H(m) {
   let s = "(Command.Handle)" + "'" + m.content.replace(/'/g, "''") + "'";
-  let v = JSON.parse(await A(m,s));
-  for (let u of v) c.createMessage(m.channel.id, F(await A(m, "(display)" + u))); 
+  let v = JSON.parse(await A(s));
+  for (let u of v) c.createMessage(m.channel.id, F(await A("(display)" + u))); 
 }
 
-c.on("messageCreate", async (m) => H(m));
-c.on("ready"        , async (_) => P(1));
+c.on("messageCreate",async(m) => H(m));
+c.on("ready"        ,async(_) => P(1));
 c.connect();
