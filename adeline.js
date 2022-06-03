3cb6b05553552f23ba82820248906432148bcaaa
awagga <awagga@mail.com>
2022-06-03 14:47:46 +0100

    whitespace

--- adeline.js
+++ adeline.js
@@ -3,16 +3,16 @@ import { spawn } from "child_process";
 import {  once } from "events"       ;
 
 const P = console.log;
-const F = (o) => { return "```" + o.slice(0, 1993) + "```" };
+const F = (s) => { return "```" + s.slice(0, 1993) + "```" };
 const c = new Eris(process.env.adeline);
 
 async function A(i) {
   let p = spawn("./apl", [i]);
   let o = "";
   
-  p.stdout.on("data",(d)=>{ o+=d.toString()});
-         p.on("exit",( )=>{ clearTimeout(t)});
-  var t = setTimeout(( )=>{ o+="EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds"
+  p.stdout.on("data",(d) => { o += d.toString()});
+         p.on("exit",( ) => { clearTimeout(t)});
+  var t = setTimeout(( ) => { o += "EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds"
     p.kill()}, 10000);
   
   await once(p, "exit");return o;
