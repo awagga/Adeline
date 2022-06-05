ea9abc6d33a7693f5431a6c74ff28ec60f580702
awagga <awagga@mail.com>
2022-06-05 19:09:25 +0100

    Style

--- adeline.js
+++ adeline.js
@@ -5,37 +5,39 @@ import   kill       from "tree-kill"
 
 let languages = { "i)": "echo", "dyalog)": "./dyalog" }
 
-async function execute(lang, source) { const child = execFile(lang, [source]); let output = ""
+async function execute(lang, source) { const child = execFile(lang, [source]); let o = ""
   const tid = setTimeout(() => {
-    output += "EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds"
-    kill(child.pid, "SIGKILL")
-  }, 10000)
+    o += "EXPRESSION TIME LIMIT EXCEEDED: Must complete within 10 seconds";
+    kill(child.pid, "SIGKILL");
+  }, 10000);
 
-  child.stdout.on("data", (data) => { output += data.toString() })
+  child.stdout.on("data", (data) => {
+    o += data.toString();
+  });
   
-  child.on("exit", () => { clearTimeout(tid) })
+  child.on("exit", () => {
+    clearTimeout(tid);
+  });
 
-  await once(child, "exit")
-
-  return output
+  await once(child, "exit") // Wait.
+  return o
 }
 
-const client = new Eris(process.env.adeline)
-client.on("messageCreate", async (msg) => {
-  if (!Object.keys(languages).some(k => msg.content.includes(k))) return;
+const c = new Eris(process.env.adeline);
+      c.on("messageCreate", async (msg) => {
+  if (!Object.keys(languages).some((k)  => msg.content.includes(k))) return; // Shortcut
 
-  if (msg.author.id == client.user.id) return;
+  if (msg.author.id == c.user.id) return;
 
-  const v = JSON.parse( // Vector of code-blocks
-    await execute(
-      "./dyalog",
+  JSON.parse( // Vector of code-blocks
+    await execute(languages["dyalog)"],
       "Command.Handle" + "'" + msg.content.replace(/'/g, "''") + " '" // Prepend space; scalar -> vector
-    ))
-
-  await v.reduce(async (promise, block) => { await promise
-    if (block[1]) // Was language prefix present?
-      client.createMessage(
-        msg.channel.id,
-        "```" + (await execute(languages[block[0]], block[1])) + "```" )
-  }, Promise.resolve())})
-client.connect()
+    )
+  ).reduce(async (promise, arr) => { await promise; let [lang, source] = arr;
+    if (source) // Is empty? I.e - was prefix present?
+      c.createMessage(msg.channel.id,
+        "```" + (await execute(languages[lang], source)) + "```"
+      );
+  }, Promise.resolve());
+});
+c.connect()
