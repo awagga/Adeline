b75f503947cbb6133e2e70c580e13a613f7f0873
awagga <awagga@mail.com>
2022-06-07 18:22:00 +0100

    Resolve issues with Truncation - account for newlines not ⎕JSON

--- adeline.js
+++ adeline.js
@@ -1,14 +1,11 @@
-import Eris from "eris";
-import { execFile } from "child_process";
+import Eris from "eris"; import { execFile } from "child_process"
 
-const c = new Eris(process.env.adeline);
-c.on("messageCreate", (m) => {
-  if (!m.content.includes("dyalog)") || m.author.id == c.user.id) return;
-  process.env.expr =
-    "Command.Handle" + "'" + m.content.replace(/'/g, "''") + "'";
-  execFile("./dyalog").stdout.on("data", (o) => { console.log(o);
+const c = new Eris(process.env.adeline)
+c.on("messageCreate", (m) => { if (m.author.id == c.user.id) return;
+  process.env.expr = "Command.Handle" + "'" + m.content.replace(/'/g, "''") + "'"
+  execFile("./dyalog").stdout.on("data", (o) =>
     c.createMessage(m.channel.id, JSON.parse(o).join("\n"))
-  });
+  )
 });c.connect();
 
 process.on("uncaughtException", (e) => console.error(e)); // Paranoia
